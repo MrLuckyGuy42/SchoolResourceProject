a6ad283f6f2933f8feb49862cf9a90b88c92e6ff
MrLuckyGuy42 <dylanleech5@gmail.com>
2024-06-10 09:16:42 +1200

    Fixed Login Box

---
+++ www/example.js
@@ -0,0 +1,22 @@
+// Let is a varible can be reasigned
+let firstName = 'Dylan'
+firstName = 1; // Reassigned varible
+// Const cant be changed
+const lastName = 'Leech'
+// Logs on the console
+console.log
+
+//primitives
+String
+Number
+Boolean
+null
+undefined
+
+//Use example
+let example5 = "Mosh" // string
+let example4 = 30; // Number
+let middleName = true; // Boolean
+let example2 = undefined; // undefined
+let example3 = null; // null
+
